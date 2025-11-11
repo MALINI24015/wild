38d07426b63e1c635ae086a08e3809d7a3d7fbcd
MALINI24015 <153347900+MALINI24015@users.noreply.github.com>
2025-11-11 13:36:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,23 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_tN28A0CFUD', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'ap-south-1_tN28A0CFU', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1jqmaon8hjl36kqgg3mlko819r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-       invokeUrl: 'https://huf30pzrpb.execute-api.ap-south-1.amazonaws.com/prods' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://huf30pzrpb.execute-api.ap-south-1.amazonaws.com/prods' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
+
+
+
+
+
+
+
+
+
+
 
 
