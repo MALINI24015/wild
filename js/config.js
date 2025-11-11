b5fa97a5852e3a416955e1285df64cbbf805e3cb
MALINI24015 <153347900+MALINI24015@users.noreply.github.com>
2025-11-11 13:02:26 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_fWDP53WQD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '38b2cus6urd1h6urdhdd2jivrs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_tN28A0CFUD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1jqmaon8hjl36kqgg3mlko819r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
